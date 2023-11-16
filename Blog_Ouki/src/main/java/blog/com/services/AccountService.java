c9abe6ede50d6870698168a8b7666d0bd98e3182
mchhichi7016 <causewaybar711@gmail.com>
2023-11-16 09:11:35 +0900

    更新911

--- Blog_Ouki/src/main/java/blog/com/services/AccountService.java
+++ Blog_Ouki/src/main/java/blog/com/services/AccountService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import blog.com.models.dao.AccountDao;
+import blog.com.models.entity.AccountEntity;
 
 @Service
 public class AccountService {
@@ -11,7 +12,27 @@ public class AccountService {
 	@Autowired
 	private AccountDao accountDao;//声明AdminDao adminDao，可以让这个类调用AdminDao接口中定义的方法，来实现对管理员数据的增删改查操作
 	
-	//新しいアカウントを作成し、データベースに保存する。/创建新账户，并将其保存到数据库中
+	//新しいアカウントを作成し、データベースに保存する
+	//创建新账户，并将其保存到数据库中
+	public boolean createAccount(String name, String email, String password) {
+		if(accountDao.findByAccountEmail(email)==null) {
+			accountDao.save(new AccountEntity(name, email, password));
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	//login处理
+	public AccountEntity checkLogin(String email, String password) {//2つのパラメータを渡す//传入两个参数
+		////アカウントが存在するかどうか、データベースに問い合わせる。
+		AccountEntity accountEntity = accountDao.findByAccountEmailAndPassword(email, password);
+		if(accountEntity == null) {
+			return null;
+		}else {
+			return accountEntity;//ログイン成功//登录成功
+		}
+	}
 	
 	
 
